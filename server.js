057d0481a598447e51cd3f2e71ae2fe260d0d10a
tcwcodes <35410637+tcwcodes@users.noreply.github.com>
2018-08-24 19:15:09 -0400

    sdlkfjhs

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
 const path = require("path");
 require("dotenv").config()
-=======
->>>>>>> parent of e9d99c8... hopefully deployed
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 const db = require("./models");
@@ -16,7 +13,6 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
 // Static
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client", "build")));
@@ -24,8 +20,6 @@ if (process.env.NODE_ENV === "production") {
 
 // app.use(express.static("client/build"));
 
-=======
->>>>>>> parent of e9d99c8... hopefully deployed
 // app.get("/populate", (req, res) => {
 //     db.Article.create({ title: "test title 2", date: "test date 2", url: "test url 2" })
 //     .then(function(dbArticle){
@@ -54,7 +48,6 @@ app.get("/api/test", (req, res) => {
     res.send("Yay");
 });
 
-<<<<<<< HEAD
 app.get("*", (req, res) => {
    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
@@ -63,8 +56,6 @@ app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "client/build/index.html"))
 // })
 
-=======
->>>>>>> parent of e9d99c8... hopefully deployed
 app.listen(PORT, function() {
     console.log(`You are listening on Port ${PORT}`);
 });
