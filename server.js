e9d99c8ab6717ce62c5cfd5b921177b86d03f888
tcwcodes <35410637+tcwcodes@users.noreply.github.com>
2018-08-24 18:29:07 -0400

    hopefully deployed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 const db = require("./models");
@@ -9,6 +10,8 @@ const db = require("./models");
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+
+app.use(express.static("client/build"));
 
 // app.get("/populate", (req, res) => {
 //     db.Article.create({ title: "test title 2", date: "test date 2", url: "test url 2" })
@@ -37,6 +40,10 @@ app.post("/api/delete", (req, res) => {
 app.get("/api/test", (req, res) => {
     res.send("Yay");
 });
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"))
+})
 
 app.listen(PORT, function() {
     console.log(`You are listening on Port ${PORT}`);
